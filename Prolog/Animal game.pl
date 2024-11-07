cf20c6c22c668b6c3eac4b15871d13742dc8ce4c
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-11-07 09:06:16 +0700

    Update Animal game.pl

--- Prolog/Animal game.pl
+++ Prolog/Animal game.pl
@@ -1,26 +1,67 @@
+person(dave, 25, male).
+person(emma, 25, female).
+person(john, 32, male).
+person(sally, 25, female).
+
+
 animal(dog, mammal, domestic, carnivore).
-animal(cat, mammal, domestic, carnivore).
+animal(cat, mammal, domestic, herbivore).
 animal(deer, mammal, forest, carnivore).
-animal(dolphin, mammal, ocean, carnivore).
+animal(dolphin, mammal, ocean, omnivore).
 animal(bear, mammal, forest, carnivore).
 
 animal(chicken, bird, farm, carnivore).
-animal(parrot, bird, forest, omnivores).
+animal(parrot, bird, forest, omnivore).
 animal(owl, bird, forest, carnivores).
-animal(penguins, bird, antarctic, carnivorous).
+animal(penguins, bird, antarctic, carnivore).
 
 animal(blob, fish, ocean, carnivore).
-animal(eel, fish, ocean, carnivore).
-animal(shark, fish, ocean, carnivore).
+animal(eel, fish, ocean, herbivore).
+animal(shark, fish, ocean, omnivore).
 animal(salmon, fish, ocean, carnivore).
 
 animal(axoloti, amphibian, water, carnivore).
-animal(frog, amphibian, water, carnivore).
-animal(lizard, amphibian, water, carnivore).
+animal(frog, amphibian, water, herbivore).
+animal(lizard, amphibian, water, omnivore).
 animal(newt, amphibian, water, carnivore).
 
-start :- write('animal game').
-asktype(Animal_name) :- animal(Animal_name, Type, _, _), write(Type).
-askhabitat(Animal_name) :- animal(Animal_name, _, Habitat, _), write(Habitat).
-askdiet(Animal_name) :- animal(Animal_name, _, _, Diet), write(Diet).
+start :-
+    write('animal game'),
+    write("i'll try to guess your animal"),
+    asktype.
+
+asktype :-
+    write("Enter the type of animal (e.g., mammal, bird, fish, amphibian): "),
+    read(Type),
+    (
+        animal(_, Type, _, _) ->
+        askfood(Type);
+        write("no"),
+        asktype
+    ).
+
+
+
+askfood(Type) :-
+    write("Is it a (herbivore, carnivore or omnivore): "),
+    read(Food),
+    (
+        animal(_, Type, _, Food) ->
+        askhabitat(Type, Food);
+        write("no"),
+        askfood(Type)
+    ).
+
+
+askhabitat(Type, Food) :-
+    write("habitat(domestic, forest, ocean, farm, antarctic, water): "),
+    read(Habitat),
+    (
+        animal(_, Type, Habitat, Food);
+        write('no')
+    ).
+
+guess(_) :-
+    write("is it: "),
+    animal(Animal, Type, Habitat, Food), write(Animal).
 
