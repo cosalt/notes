b0182bfaff1296575f11aa6e83cde75f7e525a28
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-11-13 08:55:27 +0700

    Create Sum of Numbers up to N.asm

---
+++ Assembly Language/Sum of Numbers up to N.asm
@@ -0,0 +1,16 @@
+        INP
+        STA 99
+        LDA ONE
+        STA 28
+LOOP    LDA 99
+        SUB 28
+        STA 99
+        LDA 98
+        ADD 99
+        STA 98
+        LDA 99
+        BRZ FINISH
+        BRP LOOP
+FINISH  LDA 98
+        OUT
+ONE     DAT 1
