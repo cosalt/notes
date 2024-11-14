918e53fff6368e297bce87d7b9f7818773856cff
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-11-14 09:12:03 +0700

    Create recursion division.py

---
+++ recursion division.py
@@ -0,0 +1,18 @@
+def IterativeCalculate(num):
+    arr = []
+    for i in range(1, num + 1):
+        if (num % i) == 0:
+            arr.append(i)
+    return sum(arr)
+
+
+def recursivecalculate(num, i, total=0):
+    if i != 0:
+        if (num % i) == 0:
+            print(i)
+            total = total + i
+        return rec(num, i-1, total)
+
+    return total
+
+print(recursivecalculate(10, 10))
