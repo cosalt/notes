53f009bd33255db70ffd65bc2b7da240eb53fd61
Nawin Ray Martin <91860754+cosalt@users.noreply.github.com>
2024-11-19 07:29:54 +0700

    Create bubblesort.py

---
+++ Sorting Algorithms/bubblesort.py
@@ -0,0 +1,11 @@
+def bubblesort(arr):
+    n = len(arr)
+    flag = True
+    while flag:
+        flag = False
+        for i in range(1, n):
+            if arr[i-1] > arr[i]:
+                flag = True
+                arr[i-1], arr[i] = arr[i], arr[i-1]
+                
+    return arr
